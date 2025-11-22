35921fa4d06706ba9ad36ffa8ef1ca6c5c208743
Limbo <t1029651254@163.com>
2025-11-23 00:04:53 +0800

    main getIntent

--- app/src/main/java/com/limtide/ugclite/MainActivity.java
+++ app/src/main/java/com/limtide/ugclite/MainActivity.java
@@ -1,77 +1,58 @@
 package com.limtide.ugclite;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
 import com.limtide.ugclite.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
+    private static final String TAG = "MainActivity";
+//    private static final String KEY_COUNTER = "counter_value";
     private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Intent intent = getIntent();
+        boolean isOffline = intent.getBooleanExtra("is_offline_mode", false);
+        if (isOffline) {
+            // 断网模式逻辑：比如初始化本地数据库
+            Toast.makeText(this, "当前是：断网模式", Toast.LENGTH_SHORT).show();
+        } else {
+            // 联网模式逻辑：比如初始化网络请求库
+            Toast.makeText(this, "当前是：联网模式", Toast.LENGTH_SHORT).show();
+        }
+        // 使用ViewBinding
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // 隐藏ActionBar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
         }
 
-        return super.onOptionsItemSelected(item);
+        // 设置点击事件监听器
+        setupClickListeners();
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+    private void setupClickListeners() {
+        // 拍摄按钮点击事件
+        binding.captureButton.setOnClickListener(v -> {
+            // TODO: 实现拍摄功能
+        });
+
+        // 底部导航栏点击事件
+        setupBottomNavigation();
+    }
+
+    private void setupBottomNavigation() {
+        // 首页按钮点击
+        // TODO: 实现底部导航逻辑
     }
 }

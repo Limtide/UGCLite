85f1d2b9ff60bf4b6ed16e397dfd784e2ccca392
Limbo <t1029651254@163.com>
2025-11-23 23:12:08 +0800

    探究Fragment的生命周期，

--- app/src/main/java/com/limtide/ugclite/fragment/HomeFragment.java
+++ app/src/main/java/com/limtide/ugclite/fragment/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.limtide.ugclite.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -20,15 +21,35 @@ public class HomeFragment extends Fragment {
     private SwipeRefreshLayout swipeRefreshLayout;
     private LinearLayout emptyStateLayout;
 
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        Log.d(TAG,"HomeFragment is onAttach");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG,"HomeFragment is onCreate");
+    }
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-
+        Log.d(TAG,"HomeFragment is onCreateView");
         initViews(view);
         setupRefreshListener();
 
+
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Log.d(TAG,"HomeFragment is onViewCreated");
     }
 
     private void initViews(View view) {
@@ -54,6 +75,34 @@ public class HomeFragment extends Fragment {
         if (emptyStateLayout != null) {
             emptyStateLayout.setVisibility(View.VISIBLE);
         }
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d(TAG,"HomeFragment is onDestroyView");
+        //为什么我在转移到“我”界面的时候，没有销毁UI？
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG,"HomeFragment is onStop");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"HomeFragment is onDestroy");
+        //我在退出应用的时候是否应该销毁所有创建的Fragment？还是只要销毁对应attach的Activity就可以自动销毁？
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d(TAG,"HomeFragment is onDetach");
     }
 
     private void hideEmptyState() {
